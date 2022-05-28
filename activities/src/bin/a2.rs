7f036fbe98f3001835547a29239989d10b452d32
Sam Hollyer <rsamhollyer@gmail.com>
2022-05-28 12:29:18 -0500

    updates

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,4 +8,15 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn add(num1: i32, num2: i32) -> i32 {
+    num1 + num2
+}
+
+fn display_result(result_number: i32) {
+    println!("{:?}", result_number);
+}
+
+fn main() {
+    let sum = add(2, 2);
+    display_result(sum);
+}
