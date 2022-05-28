27eacd92198e6232c3140a701cf381314e34bdce
Sam Hollyer <rsamhollyer@gmail.com>
2022-05-28 12:34:21 -0500

    updates

--- activities/src/bin/a3b.rs
+++ activities/src/bin/a3b.rs
@@ -9,14 +9,21 @@
 // * Use an if..else if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
+use std::cmp::Ordering;
 fn main() {
     let my_number: i32 = 100;
 
-    if my_number > 5 {
-        println!(">5");
-    } else if my_number < 5 {
-        println!("<5");
-    } else {
-        println!("=5");
+    // if my_number > 5 {
+    //     println!(">5");
+    // } else if my_number < 5 {
+    //     println!("<5");
+    // } else {
+    //     println!("=5");
+    // }
+
+    match my_number.cmp(&5) {
+        Ordering::Less => println!("<5"),
+        Ordering::Greater => println!(">5"),
+        Ordering::Equal => println!("=5"),
     }
 }
