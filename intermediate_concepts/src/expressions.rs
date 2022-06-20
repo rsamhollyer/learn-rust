879a31df43f4a61c8e9a8131509a20c5feaf76c4
Sam Hollyer <rsamhollyer@gmail.com>
2022-06-20 12:43:30 -0500

    updates

---
+++ intermediate_concepts/src/expressions.rs
@@ -0,0 +1,50 @@
+/*
+* Rust is an expression based language
+* -- Most of the things are evaluated and return some value
+* Expression values coalesce to a single point
+* -- Can be used for nesting logic
+*/
+
+// fn main() {
+//     let my_num: i32 = 10;
+
+//     let is_lt_5 = if my_num < 5 { true } else { false };
+//     println!("{}", is_lt_5);
+
+//     let is_lt_5 = my_num < 5;
+//     println!("{}", is_lt_5);
+
+//     let message = match my_num {
+//         1 => "one",
+//         2 => "two",
+//         3 => "three",
+//         _ => "many",
+//     };
+//     println!("{}", message);
+// }
+
+enum Menu {
+    Burger,
+    Fries,
+    Drink,
+}
+
+fn main() {
+    let paid = true;
+    let item = Menu::Drink;
+    let drink_type = "water";
+
+    let order_placed: bool = match item {
+        Menu::Drink => {
+            if drink_type == "water" {
+                true
+            } else {
+                false
+            }
+        }
+        _ => true,
+        // This is incredibly confusing
+    };
+
+    println!("{}", order_placed);
+}
