27b15b6bfcfa876b9093be1278a80ac6aeee1eca
Sam Hollyer <rsamhollyer@gmail.com>
2022-06-20 12:23:10 -0500

    updates

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -9,4 +9,22 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+fn get_coords(x: f64, y: f64) -> (f64, f64) {
+    (x, y)
+}
+
+fn y_and_five(y: f64) -> &'static str {
+    if y > 5.0 {
+        "y is greater than 5"
+    } else if y < 5.0 {
+        "y is less than 5"
+    } else {
+        "y is equal to 5"
+    }
+}
+
+fn main() {
+    let (x, y) = get_coords(3.65625650, 5.1616165);
+    println!("The x-coord is {}", x);
+    println!("The y-coord is {} and  {}", y, y_and_five(y));
+}
